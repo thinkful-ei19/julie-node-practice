9c567674349d091e7fcc61e73e492e0b6fa6140b
Elias Mason <eliasjmason@gmail.com>
2018-03-21 22:41:31 -0400

    Set process env in test setup

--- test/setup.test.js
+++ test/setup.test.js
@@ -5,7 +5,12 @@ const mongoose = require('mongoose');
 const { TEST_MONGODB_URI } = require('../config');
 
 before(function() {
+  // set the environment variable to test
+  // to suppress unwanted logging
+  process.env.NODE_ENV = 'test';
+  // clear the console between runs
   process.stdout.write('\x1Bc\n');
+
   return mongoose.connect(TEST_MONGODB_URI);
 });
 
